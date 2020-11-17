474fa4b41e35b69941b6e5865df213f6d6a6c485
Alexandria Wong <alexandriawhy@gmail.com>
2020-11-17 15:24:15 -0500

    Add auth backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  fname           :string           not null
+#  lname           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :email, :fname, :lname, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user && user.is_password?(password)
+    user
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
+# == Route Map
+#
+#      Prefix Verb   URI Pattern            Controller#Action
+#   api_users POST   /api/users(.:format)   api/users#create {:format=>:json}
+# api_session DELETE /api/session(.:format) api/sessions#destroy {:format=>:json}
+#             POST   /api/session(.:format) api/sessions#create {:format=>:json}
+#        root GET    /                      static_pages#root
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do 
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
 
   root "static_pages#root"
 end
