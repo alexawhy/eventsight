ec1808110621e7c4295b88897cbdc083301e1bbe
Alexandria Wong <alexandriawhy@gmail.com>
2020-11-19 01:53:29 -0500

    Add nav dropdown

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -10,52 +10,63 @@ class Nav extends React.Component {
     this.state = {
       visible: false
     }
+    this.expandDropdown = this.expandDropdown.bind(this);
+    this.collapseDropdown = this.collapseDropdown.bind(this);
   }
 
-  toggleDropdown() {
+  expandDropdown() {
+    this.setState({visible: true});
+  }
 
+  collapseDropdown() {
+    this.setState({visible: false});
   }
 
   render () {
     const { currentUser, logout } = this.props;
     const userIcon = <FontAwesomeIcon icon={faUser} />
+    const dropdownState = this.state.visible ? 'expand' : 'collapse';
     if (currentUser == null) {
       return (
         <nav className="nav">
-          <div className="nav-logo">
-            <a href="/">
-              <img src={window.logo} alt="eventsight" className="logo"/>
-            </a>
-          </div>
-          <div className="auth-links">
-            {/* <Link to="/signin">Sign In</Link> */}
-            <li><Link to="/signup">Sign Up</Link></li>
-            <li><Link to="/login">Log In</Link></li>
+          <div className="navbar">
+            <div className="nav-logo">
+              <a href="/">
+                <img src={window.logo} alt="eventsight" className="logo"/>
+              </a>
+            </div>
+            <ul className="auth-links">
+              {/* <Link to="/signin">Sign In</Link> */}
+              <li><Link to="/signup">Sign Up</Link></li>
+              <li><Link to="/login">Log In</Link></li>
+            </ul>
           </div>
         </nav>
       )
     } else {
       return (
         <nav className="nav">
-          <div className="nav-logo">
-            <img src={window.logo} alt="eventsight" className="logo"/>
-          </div>
-          <div className="nav-links">
-            <div className="user-dropdown">
-              <div className="user-dropdown-icon">
-                {userIcon}
-              </div>
-              <ul className="user-dropdown-list">
-                <li className="user-info">
-                  <div className="user-info-name">{currentUser.fname} {currentUser.lname}</div>
-                  <div className="user-info-email">{currentUser.email}</div>
-                </li>
-                <li className="logout">
-                  <Link to="/" onClick={logout}>Log Out</Link>
-                </li>
-              </ul>
+          <div className="navbar">
+            <div className="nav-logo">
+              <img src={window.logo} alt="eventsight" className="logo"/>
             </div>
+            <ul className="nav-links">
+              <li className="user-dropdown" onClick={this.expandDropdown}>
+                <div className="user-dropdown-icon">
+                  {userIcon}
+                </div>
+              </li>
+            </ul>
           </div>
+          <ul className={`user-dropdown-list ${dropdownState}`} tabIndex="0" onBlur={this.collapseDropdown} >
+            <li className="user-info">
+              <div className="user-info-name">{currentUser.fname} {currentUser.lname}</div>
+              <div className="user-info-email">{currentUser.email}</div>
+            </li>
+            <li className="logout">
+              <Link to="/" onClick={logout}>Log Out</Link>
+            </li>
+          </ul>
         </nav>
       )
     }
