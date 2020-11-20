a7e10e300bf1e2f562040f5a5865872c0c67ce4e
Alexandria Wong <alexandriawhy@gmail.com>
2020-11-19 21:54:05 -0500

    Add events model validations and associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id           :bigint           not null, primary key
+#  organizer_id :integer          not null
+#  category_id  :integer          not null
+#  title        :string           not null
+#  description  :text             not null
+#  venue        :string           not null
+#  start        :datetime         not null
+#  end          :datetime         not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Event < ApplicationRecord
+  validates: :organizer_id, :category_id, :title, :description, :venue, :start, :end, presence: true
+  validates: :title, uniqueness: true
+
+  belongs_to :organizer, class_name: :User
+  belongs_to :category, class_name: :Category
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id           :bigint           not null, primary key
+#  organizer_id :integer          not null
+#  category_id  :integer          not null
+#  title        :string           not null
+#  description  :text             not null
+#  venue        :string           not null
+#  start        :datetime         not null
+#  end          :datetime         not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
