f1392b9a62019ebd5685ac2b01e70920ed4d787a
Alexandria Wong <alexandriawhy@gmail.com>
2020-12-31 18:39:48 -0500

    Edit index category filter li

--- frontend/components/event/event_index/event_index.jsx
+++ frontend/components/event/event_index/event_index.jsx
@@ -3,61 +3,66 @@ import { withRouter } from 'react-router-dom';
 import EventIndexItem from './event_index_item';
 
 class EventIndex extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     index: 0
-  //   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0
+    };
 
-  //   this.filterCategory = this.filterCategory.bind(this);
-  // }
+    this.filterCategory = this.filterCategory.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchEvents();
   }
 
-  // filterCategory(index) {
-  //   this.setState({ index });
-  // }
+  filterCategory(index) {
+    this.setState({ index });
+  }
 
   render() {
-    // const { events } = this.props;
+    const { events } = this.props;
 
-    // const CATEGORY_LIST = [
-    //   'All',
-    //   'Food & Drink',
-    //   'Hobbies',
-    //   'Arts',
-    //   'Sports & Fitness'
-    // ]
+    const CATEGORY_LIST = [
+      'All',
+      'Food & Drink',
+      'Hobbies',
+      'Arts',
+      'Science & Technology',
+      'Sports & Fitness'
+    ]
 
-    // const selectedCategory = this.state.index;
-    // const categories = CATEGORY_LIST.map((category, idx) => {
-    //   const activeClass = index === selectedCategory ? 'active' : '';
+    let selectedCategory = this.state.index;
+    const categories = CATEGORY_LIST.map((category, index) => {
+      const activeClass = index === selectedCategory ? 'active' : '';
+      console.log(this.state.index)
 
-    //   return (
-    //     <li key={idx} className={activeClass} onClick={this.filterCategory(index)}>{category}</li>
-    //   )
-    // })
+      return (
+        <li key={index} className={activeClass} onClick={() => this.filterCategory(index)}>{category}</li>
+      )
+    })
 
-    // let filteredEvents;
-    // switch (this.state.index) {
-    //   case 0:
-    //     filteredEvents = events;
-    //     break;
-    //   case 1:
-    //     filteredEvents = events.filter(event => event.category_id === 6);
-    //     break;
-    //   case 2:
-    //     filteredEvents = events.filter(event => event.category_id === 8);
-    //     break;
-    //   case 3:
-    //     filteredEvents = events.filter(event => event.category_id === 10);
-    //     break;
-    //   case 4:
-    //     filteredEvents = events.filter(event => event.category_id === 14);
-    //     break;
-    // }
+    let filteredEvents;
+    switch (this.state.index) {
+      case 0:
+        filteredEvents = events;
+        break;
+      case 1:
+        filteredEvents = events.filter(event => event.category_id === 6);
+        break;
+      case 2:
+        filteredEvents = events.filter(event => event.category_id === 8);
+        break;
+      case 3:
+        filteredEvents = events.filter(event => event.category_id === 10);
+        break;
+      case 4:
+        filteredEvents = events.filter(event => event.category_id === 12);
+        break;
+      case 5:
+        filteredEvents = events.filter(event => event.category_id === 14);
+        break;
+    }
 
     return (
       <div className="event-index">
