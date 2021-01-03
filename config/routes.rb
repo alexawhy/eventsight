e5ff47a1a44dd13761da9475157caeacbc62c9c4
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-02 21:00:04 -0500

    Add bookmarks routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -26,10 +26,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do 
     resources :users, only: [:create] do 
       resources :registrations, only: [:index]
+      resources :bookmarks, only: [:index]
     end
     resource :session, only: [:create, :destroy]
     resources :events, only: [:index, :show, :create, :update, :destroy] do
       resources :registrations, only: [:create, :destroy]
+      resources :bookmarks, only: [:create, :destroy]
     end
   
     get "/organized_events", to: "events#organized_events_index"
