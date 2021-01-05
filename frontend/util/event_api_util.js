37359ff5c2642fc1eed8b44642bb34c725ccebc1
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-05 17:47:34 -0500

    Isolate bookmark_api_util

--- frontend/util/event_api_util.js
+++ frontend/util/event_api_util.js
@@ -68,24 +68,3 @@ export const fetchRegistrations = (userId) => {
   })
 }
 
-export const createBookmark = (eventId) => {
-  return $.ajax({
-    method: 'POST',
-    url: `/api/events/${eventId}/bookmarks`
-  })
-}
-
-export const deleteBookmark = (eventId) => {
-  return $.ajax({
-    method: 'DELETE',
-    url: `/api/bookmarks`,
-    data: { event_id: eventId }
-  })
-}
-
-export const fetchBookmarks = (userId) => {
-  return $.ajax({
-    method: 'GET',
-    url: `/api/users/${userId}/bookmarks`
-  })
-}
