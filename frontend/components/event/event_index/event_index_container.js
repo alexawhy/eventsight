4207e88f98972990d872bf0c9e3f22d744d522d9
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-06 16:39:37 -0500

    Edit event index container

--- frontend/components/event/event_index/event_index_container.js
+++ frontend/components/event/event_index/event_index_container.js
@@ -1,16 +1,23 @@
 import { connect } from 'react-redux';
 import { fetchEvents } from '../../../actions/event_actions';
+import { createBookmark, deleteBookmark  } from '../../../actions/bookmark_actions';
 import EventIndex from './event_index';
 
 const mapStateToProps = ({ entities: { events } }) => {
+  let currentUserId = state.session.currentUserId;
+  let bookmarks = currentUserId ? state.entities.users[state.session.currentUserId].bookmarked_events : [];
   return {
-    events: Object.values(events)
+    events: Object.values(events),
+    currentUserId,
+    bookmarks
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchEvents: () => dispatch(fetchEvents())
+    fetchEvents: () => dispatch(fetchEvents()),
+    createBookmark: (eventId) => dispatch(createBookmark(eventId)),
+    deleteBookmark: (eventId) => dispatch(deleteBookmark(eventId))
   }
 }
 
