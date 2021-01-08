95ffd7c447cc0ff204946c5d294099775cd89060
Alexandria Wong <alexandriawhy@gmail.com>
2021-01-08 18:52:02 -0500

    Add footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faAngellist } from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends React.Component {
   render() {
@@ -11,6 +12,12 @@ class Footer extends React.Component {
         </a>
         <a href="https://www.linkedin.com/in/alexandria-hy-wong/" target="_blank">
           <FontAwesomeIcon className="footer-icon" icon={faLinkedin} />
+        </a>
+        <a href="https://angel.co/u/alexandria-wong" target="_blank">
+          <FontAwesomeIcon className="footer-icon" icon={faAngellist} />
+        </a>
+        <a href="https://alexandria-wong.com/" target="_blank">
+          <FontAwesomeIcon className="footer-icon" icon={faAddressCard} />
         </a>
       </div>
     )
